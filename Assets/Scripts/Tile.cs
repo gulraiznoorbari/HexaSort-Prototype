b4014e5ebc21ffb15316cdbdc19d0a1ca6c0cc33
Gulraiz Noor Bari ⚡ <78252747+gulraiznoorbari@users.noreply.github.com>
2024-03-01 08:21:56 +0500

    Small fix

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -89,6 +89,7 @@ public class Tile : MonoBehaviour
         // small delay when reached on above of matched tile:
         yield return new WaitForSeconds(0.2f);
 
+        GridManager.instance.MarkCellUnOccupied(targetPosition);
         GameObject matchedTile = GridManager.instance.GetTileAtPosition(targetPosition);
         if (matchedTile != null) Destroy(matchedTile);
         Destroy(gameObject);
